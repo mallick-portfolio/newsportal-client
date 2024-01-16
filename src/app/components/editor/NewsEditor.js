00f06e15f52d919fd8fe985a206caa1f9134204c
mallick-portfolio <tamal1714019@gmail.com>
2024-01-16 22:44:51 +0600

    update ...

--- src/app/components/editor/NewsEditor.js
+++ src/app/components/editor/NewsEditor.js
@@ -1,62 +1,9 @@
-import { useState, useMemo } from "react";
-import dynamic from "next/dynamic";
-import "react-quill/dist/quill.snow.css";
+import React from "react";
 
 const NewsEditor = () => {
-  const [value, setValue] = useState("");
-  const ReactQuill = useMemo(
-    () => dynamic(() => import("react-quill"), { ssr: false }),
-    []
-  );
-
-  const modules = useMemo(
-    () => ({
-      toolbar: {
-        container: [
-          [{ header: [1, 2, 3, 4, false] }],
-          ["bold", "italic", "underline", "blockquote"],
-          // [{ color: [] }],
-          [
-            { list: "ordered" },
-            { list: "bullet" },
-            { indent: "-1" },
-            { indent: "+1" },
-          ],
-          ["link", "image"],
-          ["clean"],
-        ],
-      },
-      clipboard: {
-        matchVisual: true,
-      },
-    }),
-    []
-  );
-
-  const formats = [
-    "header",
-    "bold",
-    "italic",
-    "underline",
-    "strike",
-    "blockquote",
-    "list",
-    "bullet",
-    "indent",
-    "link",
-    "image",
-  ];
-
   return (
     <div>
-      {/*... */}
-      <ReactQuill
-        theme="snow"
-        value={value}
-        modules={modules}
-        formats={formats}
-        onChange={setValue}
-      />
+      <h1>hello world</h1>
     </div>
   );
 };
